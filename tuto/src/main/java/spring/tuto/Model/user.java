0563826447b244f00bf384c838e2e6940d8501e2
LukaMoraes <170033178+LukaMoraes@users.noreply.github.com>
2024-06-15 05:19:36 -0300

    Create User

---
+++ tuto/src/main/java/spring/tuto/Model/user.java
@@ -0,0 +1,10 @@
+package spring.tuto.Model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class user {
+    public user() {
+        // TODO Auto-generated method stub
+    }
+}
---
+++ tuto/src/test/java/spring/tuto/SpringTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.tuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
